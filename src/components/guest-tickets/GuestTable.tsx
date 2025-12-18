44f2d414ad98eefb003659e062545f2fb0cf5155
thadoos <thaddeus.ong.23@ucl.ac.uk>
2025-12-18 05:40:46 +0000

    fixed background color when no guests in tabe

--- src/components/guest-tickets/GuestTable.tsx
+++ src/components/guest-tickets/GuestTable.tsx
@@ -25,7 +25,7 @@ export const GuestTable = ({
 }: GuestTableProps) => {
   if (guests.length === 0) {
     return (
-      <div className="rounded-2xl bg-card-secondary-background p-12 text-center">
+      <div className="rounded-2xl bg-card-background p-12 text-center">
         <div className="mx-auto mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-primary/10">
           <Inbox className="h-8 w-8 text-primary" />
         </div>
