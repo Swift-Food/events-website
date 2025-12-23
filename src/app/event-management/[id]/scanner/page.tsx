6b3e5878ae5b84d2a10bc05ec7673d4547baba6e
Momo4811 <mohamedmohamed4881@gmail.com>
2025-12-23 13:52:56 +0000

    Remove max-height constraint from scanner

--- src/app/event-management/[id]/scanner/page.tsx
+++ src/app/event-management/[id]/scanner/page.tsx
@@ -340,10 +340,10 @@ export default function CheckInPage() {
             </div>
           </div>
 
-          {/* Scanner Container - uses detected camera ratio, square-ish on desktop */}
+          {/* Scanner Container - uses detected camera ratio or 4:3 default */}
           <div
             className={`relative bg-black overflow-hidden ${
-              !cameraAspectRatio ? "aspect-[4/3] sm:aspect-[4/3] sm:max-h-[400px]" : "sm:max-h-[400px]"
+              !cameraAspectRatio ? "aspect-[4/3]" : ""
             }`}
             style={cameraAspectRatio ? { aspectRatio: `${cameraAspectRatio}` } : undefined}
           >
