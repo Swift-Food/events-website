de026948ff28d913d361f8a089184156494e94cf
thadoos <thaddeus.ong.23@ucl.ac.uk>
2026-02-02 16:25:45 +0000

    Fixed placeholder image for event management page

--- src/components/event-management/tabs/catering/index.ts
+++
@@ -1,6 +0,0 @@
-export { BundleCard } from "./BundleCard";
-export { CreateYourOwnCard } from "./CreateYourOwnCard";
-export { EditPickupContactModal } from "./EditPickupContactModal";
-export { ExistingOrderView } from "./ExistingOrderView";
-export { MealSessionCard } from "./MealSessionCard";
-export { OrderTimeline } from "./OrderTimeline";
--- src/components/event-management/tabs/index.ts
+++
@@ -1,5 +0,0 @@
-export { OverviewTab } from "./OverviewTab";
-export { GuestsTab } from "./GuestsTab";
-export { RegistrationTab } from "./RegistrationTab";
-export { TeamTab } from "./TeamTab";
-export {CateringTab} from "./CateringTab";
